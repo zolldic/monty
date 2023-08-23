b6c305caa7ac9c084071b21d2fd7c65ef3edd078
sai <ojiip95@gmail.com>
2023-08-23 21:44:05 +0200

    implement the push()

--- _push.c
+++ _push.c
@@ -8,15 +8,33 @@
 
 void _push(stack_t **st, obj_t *object)
 {
-
 	char **ob;
-
-	(void)object;
-	(void)st;
-
+	stack_t *tmp;
 
 	ob = malloc(sizeof(char **));
+	if (!ob)
+		fprintf(stderr, "Error: malloc failed");
+
 	ob = _tokenize(object->str, " \t");
+
+	tmp = malloc(sizeof(stack_t));
+	if (!tmp)
+		fprintf(stderr, "Error: malloc failed");
+
+	tmp->n = atoi(ob[1]);
+
+	if ((*st) == NULL)
+	{
+		tmp->next = NULL;
+		tmp->prev = NULL;
+		(*st) = tmp;
+	}
+	else
+	{
+		(*st)->next = tmp;
+		tmp->prev = (*st);
+		(*st) = tmp;
+	}
 
 	object->flag = NOERR;
 
